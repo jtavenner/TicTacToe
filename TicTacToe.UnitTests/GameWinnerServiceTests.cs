38300da042fa2b9875a67c9735f969e77f8e8ade
jtavenner <username@users.noreply.github.com>
2018-05-22 23:25:31 -0400

    Added Setup method to unit test file

--- TicTacToe.UnitTests/GameWinnerServiceTests.cs
+++ TicTacToe.UnitTests/GameWinnerServiceTests.cs
@@ -11,29 +11,33 @@ namespace TicTacToe.UnitTests
     [TestFixture]
     public class GameWinnerServiceTests
     {
+        IGameWinnerService _gameWinnerService;
+
+        [SetUp]
+        public void SetupUnitTest()
+        {
+            _gameWinnerService = new GameWinnerService();
+        }
+
         [Test]
         public void NeitherPlayerHasThreeInARow()
         {
-            IGameWinnerService gameWinnerService;
-            gameWinnerService = new GameWinnerService();
-            const char expected = ' ';
+           const char expected = ' ';
             var gameBoard = new char[3, 3] { { ' ', ' ', ' '},
                                              { ' ', ' ', ' '},
                                              { ' ', ' ', ' '} };
-            var actual = gameWinnerService.Validate(gameBoard);
+            var actual = _gameWinnerService.Validate(gameBoard);
             Assert.AreEqual(expected, actual);
         }
 
         [Test]
         public void PlayerWithAllSpacesInTopRowIsWinner()
         {
-            IGameWinnerService gameWinnerService;
-            gameWinnerService = new GameWinnerService();
-            const char expected = 'X';
+           const char expected = 'X';
             var gameBoard = new char[3, 3] { { expected, expected, expected},
                                              { ' ', ' ', ' '},
                                              { ' ', ' ', ' '} };
-            var actual = gameWinnerService.Validate(gameBoard);
+            var actual = _gameWinnerService.Validate(gameBoard);
             Assert.AreEqual(expected.ToString(), actual.ToString());
         }
     }
