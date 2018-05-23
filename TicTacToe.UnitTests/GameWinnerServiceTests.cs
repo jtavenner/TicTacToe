732621c7924772daba3a7e465aa4e42080c54fce
jtavenner <username@users.noreply.github.com>
2018-05-22 23:04:34 -0400

    First Commit

---
+++ TicTacToe.UnitTests/GameWinnerServiceTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace TicTacToe.UnitTests
+{
+    [TestFixture]
+    public class GameWinnerServiceTests
+    {
+        [Test]
+        public void NeitherPlayerHasThreeInARow()
+        {
+            IGameWinnerService gameWinnerService;
+            gameWinnerService = new GameWinnerService();
+            const char expected = ' ';
+            var gameBoard = new char[3, 3] { { ' ', ' ', ' '},
+                                             { ' ', ' ', ' '},
+                                             { ' ', ' ', ' '} };
+            var actual = gameWinnerService.Validate(gameBoard);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
